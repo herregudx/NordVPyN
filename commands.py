ffb0c399a2a8cfb74cd76eebc9d6e3ed6fab3d33
herregudx <herregud80@hotmail.com>
2024-03-28 15:43:30 +0100

    Divided functionality into several files. Hopefully this helps to keep everything organized.

---
+++ commands.py
@@ -0,0 +1,39 @@
+import subprocess
+
+def login():
+    # Login to NordVPN
+    subprocess.run(["nordvpn", "login"])
+
+
+def connect(server_country):
+    # Connect to VPN
+    print(server_country)
+    if server_country != "":
+        subprocess.run(["nordvpn", "connect", server_country])
+    else:
+        subprocess.run(["nordvpn", "connect"])
+
+
+def disconnect():
+    # Disconnect from VPN
+    subprocess.run(["nordvpn", "disconnect"])
+
+
+def show_status():
+    # Show status of VPN connection
+    subprocess.run(["nordvpn", "status"])
+
+
+def show_account_info():
+    # Show account info
+    subprocess.run(["nordvpn", "account"])
+
+
+def show_settings():
+    # Show settings
+    subprocess.run(["nordvpn", "settings"])
+
+
+def show_version():
+    # Show NordVPN version
+    subprocess.run(["nordvpn", "version"])
---
+++ usermenu.py
@@ -0,0 +1,77 @@
+import ascii_art
+#import main
+import subprocess
+import commands
+import country
+
+
+# Variable to specify which country to connect to. This string can be left empty
+# to let the nordvpn-client select automatically.
+server_country = ""
+
+
+def show_basic_menu():
+    clear_screen()
+    ascii_art.draw_logo()
+    # Show the user a menu with choices
+    while True:
+        menu_choice = input("\n[C]onnect, [D]isconnect, [S]tatus, [M]ore, [Q]uit: ")
+        clear_screen()
+        ascii_art.draw_logo()
+        if menu_choice.upper() == "C":
+            commands.connect(server_country)
+        elif menu_choice.upper() == "D":
+            commands.disconnect()
+        elif menu_choice.upper() == "S":
+            commands.show_status()
+        elif menu_choice.upper() == "M":
+            show_advanced_menu()
+        elif menu_choice.upper() == "Q":
+            exit()
+
+
+def show_advanced_menu():
+    clear_screen()
+    ascii_art.draw_logo()
+    # Show the user a menu with more advanced choices
+    while True:
+        menu_choice = input("\n[A]ccount, [S]ettings, [C]onfig, [V]ersion, [B]ack: ")
+        clear_screen()
+        ascii_art.draw_logo()
+        if menu_choice.upper() == "A":
+            commands.show_account_info()
+        elif menu_choice.upper() == "S":
+            commands.show_settings()
+        elif menu_choice.upper() == "C":
+            show_config_menu()
+        elif menu_choice.upper() == "V":
+            commands.show_version()
+        elif menu_choice.upper() == "B":
+            show_basic_menu()
+
+
+def show_config_menu():
+    clear_screen()
+    ascii_art.draw_logo()
+    # Show the user a menu with choices
+    while True:
+        menu_choice = input("\n[L]ogin, [C]ountry, [B]ack: ")
+        clear_screen()
+        ascii_art.draw_logo()
+        if menu_choice.upper() == "L":
+            commands.login()
+        elif menu_choice.upper() == "C":
+            set_server_country()
+        elif menu_choice.upper() == "B":
+            show_basic_menu()
+
+
+def set_server_country():
+    # Attempts to set the server_country variable
+    global server_country
+    server_country = country.select_server_country()
+
+
+def clear_screen():
+    # Clear the screen to keep it tidy
+    subprocess.run(["clear"])
